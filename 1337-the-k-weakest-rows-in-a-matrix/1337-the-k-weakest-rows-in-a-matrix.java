2f1dcaf7fa29d7e33f93629cba36e21d85d60c82
codinghuslter <110764014+codinghuslter@users.noreply.github.com>
2022-08-27 17:58:52 +0530

    Time: 1 ms (99.83%), Space: 43.1 MB (98.68%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,41 +1,42 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int[] index=new int[mat.length];
-        int[] soldiers=new int[mat.length];
-        int[] kweak=new int[k];
+        int m= mat.length;
+        int[] index = new int[m];
+        int[] res = new int[m];
         
-        for(int i=0;i<mat.length;i++)
-        {
-            int count=0;
-            for(int j=0;j<mat[i].length;j++)
-            {
+        for(int i=0 ; i<m ; i++){
+            int cnt=0;
+            for(int j=0 ; j<mat[i].length ; j++){
                 if(mat[i][j]==1)
-                    count+=1;
+                    cnt++;
             }
+            res[i]=cnt;
             index[i]=i;
-            soldiers[i]=count;
         }
         
-        for(int i=0;i<soldiers.length-1;i++)
+        for(int i=0 ; i<res.length-1 ; i++ )
         {
-            for(int j=0;j<(soldiers.length-1-i);j++)
+            for(int j=0 ; j<(res.length-1-i) ; j++)
             {
-                if(soldiers[j]>soldiers[j+1])
+                if(res[j]>res[j+1])
                 {
-                    int t=soldiers[j];
-                    soldiers[j]=soldiers[j+1];
-                    soldiers[j+1]=t;
+                    int temp1 = res[j];
+                    res[j]=res[j+1];
+                    res[j+1]=temp1;
                     
-                    int t1=index[j];
+                    int temp2 = index[j];
                     index[j]=index[j+1];
-                    index[j+1]=t1;
+                    index[j+1]=temp2;
                 }
             }
         }
-        for(int i=0;i<k;i++)
+        int[] kweak = new int[k];
+        
+        for(int i=0 ; i<k ; i++)
         {
-            kweak[i]=index[i];
+            kweak[i] = index[i];
         }
+        
         return kweak;
     }
 }
